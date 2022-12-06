7ac73e80735072fcf36ee6934302c681711c2fc2
Glenn Vanderburg <glv@vanderburg.org>
2022-12-06 10:20:24 -0600

    add source template

---
+++ day-template.clj
@@ -0,0 +1,11 @@
+(ns aoc2022.day00
+  (:require [clojure.set :as set]
+            [clojure.string :as str]))
+
+(defn input-lines []
+  (line-seq (java.io.BufferedReader. *in*)))
+
+(defn run [args]
+  (let [a 1]
+    (println "star 1:")
+    (println "star 2:"))
