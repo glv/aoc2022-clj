e6b90cfd7f9e2cd3877e020f0b2f52c0b6103de7
Glenn Vanderburg <glv@vanderburg.org>
2022-12-12 18:49:01 -0600

    day 12

--- src/aoc2022/day12.clj
+++ src/aoc2022/day12.clj
@@ -43,15 +43,12 @@
                        end? \z
                        :else char)
               cell {:coord [x y]
-                    :start start?
-                    :end end?
                     :height height}
               start (cond start start start? [x y] :else nil)
               end (cond end end end? [x y] :else nil)]
           (recur (conj cells cell) (inc index) start end chars))
 
-        [cells start end])
-      )))
+        [cells start end]))))
 
 (defn build-grid [lines]
   (let [rows (count lines)
@@ -61,39 +58,34 @@
 
 (defn process-frontier [grid frontier path-length]
   (letfn [(eligible-neighbors [grid cell]
-            (let [max-height (inc (int (:height cell)))
+            (let [min-height (dec (int (:height cell)))
                   neighbor-coords (neighbors grid cell)
                   neighbor-cells (map (partial grid-cell grid) neighbor-coords)]
-              (filter #(<= (int (:height %)) max-height) neighbor-cells)))]
+              (filter #(>= (int (:height %)) min-height) neighbor-cells)))]
     (let [next-frontier (->> frontier
                              (mapcat (partial eligible-neighbors grid))
                              (into #{})
                              (filter #(nil? (:step-num %))))]
       next-frontier)))
 
-(defn dijkstra [{:keys [cells start end] :as grid}]
-  (loop [grid (assoc-in grid [:cells (cell-index grid start) :step-num] 0)
+(defn dijkstra [{:keys [cells end] :as grid} found-fn]
+  (loop [grid (assoc-in grid [:cells (cell-index grid end) :step-num] 0)
          path-length 1
-         [frontier & frontiers] [[(grid-cell grid start)]]]
-    (if (or (and frontier (not (empty? frontier)))
-            (nil? (:step-num (grid-cell grid end)))
-            )
+         [frontier & frontiers] [[(grid-cell grid end)]]]
+    (if (some found-fn frontier)  ; #(= start (:coord %))
+      (dec path-length)
+
       (let [next-frontier (process-frontier grid frontier path-length)]
         (recur (reduce #(assoc-in %1 [:cells (cell-index grid (:coord %2)) :step-num] path-length)
                        grid
                        next-frontier)
                (inc path-length)
-               (conj frontiers next-frontier)))
-
-      (:step-num (grid-cell grid end)))
-    )
-  )
+               (conj frontiers next-frontier))))))
 
 (defn run [args]
-  (let [shortest-path-length (->> input-lines
-                                  build-grid
-                                  dijkstra
-                                  )]
-    (println "star 1:" shortest-path-length)
-    (println "star 2:"))
+  (let [grid (build-grid input-lines)
+        shortest-path-from-start (dijkstra grid #(= (:start grid) (:coord %)))
+        shortest-path-from-any-a (dijkstra grid #(= \a (:height %)))]
+    (println "star 1:" shortest-path-from-start)
+    (println "star 2:" shortest-path-from-any-a))
   )
