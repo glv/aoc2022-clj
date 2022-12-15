88ed58913e6a836a7478c70d39afbfa899bededc
Glenn Vanderburg <glv@vanderburg.org>
2022-12-15 08:45:23 -0600

    dbg macro should return the value of the passed expr

--- day-template.clj
+++ day-template.clj
@@ -4,11 +4,12 @@
 
 (defmacro dbg
   "Print to *out* the value of an expr, labeled with the expression itself.
-  Can be called in four ways:
-  * (dbg expr) -- prints 'expr: value'
-  * (dbg 2 expr) -- prints '  expr: value' (the int is the level of indentation)
-  * (dbg \"msg\" expr) -- prints 'msg: value' (overrides the default label)
-  * (dbg 2 \"msg\" expr) -- prints '  msg: value' (both indent and label override)"
+Can be called in four ways:
+* (dbg expr) -- prints 'expr: value'
+* (dbg 2 expr) -- prints '  expr: value' (the int is the level of indentation)
+* (dbg \"msg\" expr) -- prints 'msg: value' (overrides the default label)
+* (dbg 2 \"msg\" expr) -- prints '  msg: value' (both indent and label override)
+Returns the value of expr."
   [& args]
   (let [[lvl msg val] (condp = (count args)
                         1 [0 (first args) (first args)]
@@ -16,7 +17,9 @@
                             [(first args) (second args) (second args)]
                             [0 (first args) (second args)])
                         3 args)]
-    `(println ~(str (str/join (repeat lvl " ")) msg ":") ~val)))
+    `(let [val# ~val]
+       (println ~(str (str/join (repeat lvl " ")) msg ":") val#)
+       val#)))
 
 (defonce input-lines
   (line-seq (java.io.BufferedReader. *in*)))
