66fce1258830b9371c952de49a9f34989f7e6d00
Glenn Vanderburg <glv@vanderburg.org>
2022-12-14 18:58:29 -0600

    day 14 (partial)

---
+++ src/aoc2022/day14.clj
@@ -0,0 +1,145 @@
+(ns aoc2022.day14
+  (:require [clojure.set :as set]
+            [clojure.string :as str]))
+
+(defmacro dbg
+  "Print to *out* the value of an expr, labeled with the expression itself.
+  Can be called in four ways:
+  * (dbg expr) -- prints 'expr: value'
+  * (dbg 2 expr) -- prints '  expr: value' (the int is the level of indentation)
+  * (dbg \"msg\" expr) -- prints 'msg: value' (overrides the default label)
+  * (dbg 2 \"msg\" expr) -- prints '  msg: value' (both indent and label override)"
+  [& args]
+  (let [[lvl msg val] (condp = (count args)
+                        1 [0 (first args) (first args)]
+                        2 (if (number? (first args))
+                            [(first args) (second args) (second args)]
+                            [0 (first args) (second args)])
+                        3 args)]
+    `(println ~(str (str/join (repeat lvl " ")) msg ":") ~val)))
+
+(defonce input-lines
+  (line-seq (java.io.BufferedReader. *in*)))
+
+(defn cell-index [{:keys [xmin ymin cols cells]} [x y]]
+  (let [x (- x xmin)
+        y (- y ymin)]
+    (+ (* y cols) x)))
+
+(defn grid-cell [{cells :cells :as grid} [x y]]
+  (get cells (cell-index grid [x y])))
+
+(defn off-grid? [{:keys [xmin xmax ymax]} [x y]]
+  (or (< x xmin)
+      (> x xmax)
+      (> y ymax)))
+
+;; (defn neighbor [grid cell direction]
+;;   (let [[col row] (:coord cell)
+;;         [mcol mrow] (map dec (:size grid))]
+;;     (case direction
+;;       :north (when (> row 0) [col (dec row)])
+;;       :west  (when (> col 0) [(dec col) row])
+;;       :south (when (< row mrow) [col (inc row)])
+;;       :east  (when (< col mcol) [(inc col) row]))))
+;;
+;; (defn neighbors
+;;   ([grid cell] (neighbors grid cell [:north :south :east :west]))
+;;   ([grid cell directions]
+;;    (filter identity (map #(neighbor grid cell %) directions))))
+
+(defn print-grid [{:keys [xmin xmax cells] :as grid}]
+  (let [row-len (inc (- xmax xmin))]
+    (doseq [[i row] (map vector (range) (->> cells
+                                             (partition row-len)
+                                             (map #(map :v %))
+                                             (map str/join)))]
+      (println i row))))
+
+(defn coords-from-line [line]
+  (let [point-strs (str/split line #" -> ")]
+    (->> point-strs
+         (map #(str/split % #","))
+         (map #(map read-string %))
+         (map vec))))
+
+(defn build-grid [{:keys [xmin xmax ymin ymax] :as state}]
+  (let [cols (inc (- xmax xmin))
+        rows (inc (- ymax ymin))
+        cells (vec (repeat (* cols rows) {:v \.}))]
+    (assoc state :cols cols :rows rows :cells cells)))
+
+(defn expand-dims [{:keys [xmin xmax ymin ymax] :as state} line]
+  (letfn [(newdim [minormax pluck-fn dim coords]
+            (apply minormax (->> coords
+                                 (map pluck-fn)
+                                 (cons dim)
+                                 (filter identity))))]
+    (let [coords (coords-from-line line)
+          xmin (newdim min first xmin coords)
+          xmax (newdim max first xmax coords)
+          ymin (newdim min second ymin coords)
+          ymax (newdim max second ymax coords)]
+      {:xmin xmin :xmax xmax :ymin ymin :ymax ymax})))
+
+(defn find-dims [lines]
+  (let [state {:xmin nil :xmax nil :ymin 0 :ymax nil}]
+    (reduce expand-dims state lines)))
+
+(defn segments-from-line [line]
+  (let [coords (coords-from-line line)]
+    (map vector coords (rest coords))))
+
+(defn parse-segments [lines]
+    (mapcat segments-from-line lines))
+
+(defn add-segment [grid segment]
+  (let [[start end] (sort segment)
+        advance-fn (if (= (first start) (first end))
+                     #(vector (first %) (inc (second %)))
+                     #(vector (inc (first %)) (second %)))]
+    (loop [grid grid coord start]
+      (let [new-grid (assoc-in grid [:cells (cell-index grid coord) :v] \#)]
+        (if (= end coord)
+          new-grid
+          (recur new-grid (advance-fn coord)))))))
+
+(defn add-paths [{cells :cells :as grid} lines]
+  (let [grid (assoc-in grid
+                       [:cells (cell-index grid [500 0]) :v]
+                       \+)
+        segments (parse-segments lines)
+        ]
+    (reduce add-segment grid segments)))
+
+(defn trickle-sand-unit [grid]
+  (letfn [(candidate-status [grid [x y :as coord]]
+            (if (off-grid? grid coord)
+              :abyss
+              (condp = (:v (grid-cell grid coord))
+                \. :free
+                \# :blocked
+                \o :blocked)))
+          (next-sand-pos [grid [x y :as sand-pos]]
+            (let [candidates [[x (inc y)]
+                              [(dec x) (inc y)]
+                              [(inc x) (inc y)]]
+                  statuses (map candidate-status candidates)
+                  winner (->> statuses
+                              (map ))]
+              ))
+          ]
+    (loop [grid grid sand-pos [500 0]]
+      )))
+
+(defn trickle-sand [grid]
+  (assoc grid :sand-units 0))
+
+(defn run [args]
+  (let [dims (find-dims input-lines)
+        grid (build-grid dims)
+        grid (add-paths grid input-lines)
+        grid (trickle-sand grid)
+        _ (print-grid grid)]
+    (println "star 1:" (:sand-units grid))
+    (println "star 2:")))
